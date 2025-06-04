9c3e751e2f46334639c93eb49ae22000362ca661
OXR <RRR777x@icloud.com>
2025-06-04 11:06:50 +0000

    Added the site detector template

---
+++ content.js
@@ -0,0 +1,23 @@
+const site = (() => {
+    if (location.hostname.includes('discord.com')) return 'discord';
+    if (location.hostname.includes('axiom.trade')) return 'axiom';
+    if (location.hostname.includes('nova.trade')) return 'nova';
+    if (location.hostname.includes('photon-sol.tinyastro.io')) return 'photon';
+    if (location.hostname.includes('uxento.io')) return 'uxento';
+})();
+
+function eunoia() {
+    if (site === 'discord') {
+
+    } else if (site === 'axiom') {
+        
+    } else if (site === 'nova') {
+        
+    } else if (site === 'photon') {
+        
+    } else if (site === 'uxento') {
+
+    }
+}
+
+eunoia();
